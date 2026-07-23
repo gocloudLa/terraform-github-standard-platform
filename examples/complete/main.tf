e1b6db34a2e74fd64bebb4af835a7ae5c0a84b20
Federico Midaglia <fmidaglia@gocloud.la>
2026-07-23 18:33:11 -0300

    chore(example): add examples/complete

---
+++ examples/complete/main.tf
@@ -0,0 +1,39 @@
+module "wrapper_organization" {
+  source = "../../modules/organization"
+
+  metadata = {
+    environment  = "Production"
+    github_owner = "example-org"
+
+    key = {
+      company = "dmc"
+      env     = "prd"
+      layer   = "organization"
+    }
+  }
+
+  organization_parameters = {}
+  organization_defaults   = {}
+  team_parameters          = {}
+  team_defaults           = {}
+}
+
+module "wrapper_project" {
+  source = "../../modules/project"
+
+  metadata = {
+    environment  = "Production"
+    project      = "Common"
+    github_owner = "example-org"
+
+    key = {
+      company = "dmc"
+      env     = "prd"
+      project = "common"
+      layer   = "project"
+    }
+  }
+
+  repository_parameters = {}
+  repository_defaults   = {}
+}
